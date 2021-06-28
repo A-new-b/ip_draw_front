6f2a911a5348cf56fde16ce9a863496680b5ed36
QinHuoBin <qinhuobin@outlook.com>
2021-06-28 16:35:16 +0800

    2020-06-28 解决跨域问题

--- src/components/echarts.vue
+++ src/components/echarts.vue
@@ -14,35 +14,62 @@
 import ECharts from "vue-echarts";
 import "echarts/lib/chart/line";
 import "echarts/lib/component/polar";
+import axios from "axios";
 
 export default {
   components: {
-    "v-chart": ECharts,
+    "v-chart": ECharts
   },
   data() {
     return {
       option: {
         xAxis: {
           type: "category",
-          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
         },
         yAxis: {
-          type: "value",
+          type: "value"
         },
         series: [
           {
             data: [150, 230, 224, 218, 135, 147, 260],
-            type: "line",
-          },
-        ],
-      },
+            type: "line"
+          }
+        ]
+      }
     };
   },
-  methods:{
-    startListen(){
-
-    }
+  mounted() {
+    this.updateData();
   },
+  methods: {
+    startListen() {
+
+    },
+    endListen() {
+
+    },
+    updateData() {
+      let _this = this;
+      axios
+          .get("/api/init")
+          .then(response => (this.data = response.data));
+      this.timer = setInterval(() => {
+        //console.log(_this)
+        // console.log(this)
+        //console.log(_this.option.series.);//["data"][0]++;
+        //console.log(JSON.parse(JSON.stringify(_this.option.series)));
+
+        // 获取真实数据
+        //console.log(JSON.parse(JSON.stringify(_this.option.series[0])));
+
+        this.option.series[0].data[0] += 10;
+        //this._init()
+        //console.log(_this.setOption)
+        window.a = _this;
+      }, 1000);
+    }
+  }
 
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   transpileDependencies: ["vuetify", "vue-echarts", "resize-detector"],
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',//vue访问后台服务的端口
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }
+    }
+  }
 };
