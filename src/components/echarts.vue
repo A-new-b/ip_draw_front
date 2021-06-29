dc6e67007effdbecc18b68a2d9635ba94307a012
QinHuoBin <qinhuobin@outlook.com>
2021-06-29 22:34:57 +0800

    2020-06-29 完成两个表格

--- src/components/echarts.vue
+++ src/components/echarts.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <v-chart :options="option" style="width: 100%; height: 400px" />
+    <div style="float:left;width: 50%;">
+      <v-chart autoresize :options="option_for_second_num" style="float:left;width: 100%; height: 400px " />
+      <div align="center">总数据包：{{ now_num }}</div>
+    </div>
+
+    <div style="float:left;width: 50%;">
+      <v-chart autoresize :options="option_for_second_bytes" style="float:left;width: 100%; height: 400px" />
+      <div align="center"> 总流量：{{ now_bytes }}</div>
+    </div>
+
+
     <v-container style="display: flex;flex-direction: row;justify-content: center">
       <v-btn color="primary" style="margin: 1%">开始监听</v-btn>
       <v-btn color="primary" style="margin: 1%">停止监听</v-btn>
@@ -14,6 +24,8 @@
 import ECharts from "vue-echarts";
 import "echarts/lib/chart/line";
 import "echarts/lib/component/polar";
+import 'echarts/lib/component/title'
+import 'echarts/lib/component/tooltip'
 import axios from "axios";
 
 export default {
@@ -22,18 +34,32 @@ export default {
   },
   data() {
     return {
-      option: {
+      now_num: undefined,
+      now_bytes: undefined,
+      option_for_second_num: {
+        title: {
+          left: 'center',
+          text: '每秒处理数据包/个',
+        },
         tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            animation: false
-          }
+          trigger: "axis",
+
+          // axisPointer: {
+          //   type: 'cross',
+          //   animation: false,
+          //   label: {
+          //     backgroundColor: '#ccc',
+          //     borderColor: '#aaa',
+          //     borderWidth: 1,
+          //     shadowBlur: 0,
+          //     shadowOffsetX: 0,
+          //     shadowOffsetY: 0,
+          //
+          //     color: '#222'
+          //   }
+          // },
         },
         xAxis: {
-          // type: 'category', // category为一级分类,适用于离散的类目数据
-          // boundaryGap: false,  // 无间隙
-          // data: Array.from(Array(100), (v,k) =>k)
-
           type: "time",
           splitLine: {
             show: false
@@ -49,18 +75,54 @@ export default {
         series: [
           {
             // data: [150, 230, 224, 218, 135, 147, 260],
+            name:"每秒数据包",
             data: [],
             showSymbol: true,
             hoverAnimation: false,
             type: "line"
           }
         ]
+      },
+      option_for_second_bytes: {
+        title: {
+          left: 'center',
+          text: '每秒处理数据/字节',
+        },
+        tooltip: {
+          trigger: "axis",
+          // axisPointer: {
+          //   animation: false
+          // }
+        },
+        xAxis: {
+          type: "time",
+          splitLine: {
+            show: false
+          }
+        },
+        yAxis: {
+          type: "value",
+          boundaryGap: [0, "100%"],
+          splitLine: {
+            show: true
+          }
+        },
+        series: [
+          {
+            // data: [150, 230, 224, 218, 135, 147, 260],
+            name:"每秒字节",
+            data: [],
+            showSymbol: true,
+            hoverAnimation: false,
+            type: "line",
+          }
+        ]
       }
     };
   },
   mounted() {
-    this.initalizeData()
-    this.updateData();
+    this.initalizeData();
+    this.setIntervalForUpdate();
   },
   methods: {
     startListen() {
@@ -69,15 +131,10 @@ export default {
     endListen() {
 
     },
-    updateData() {
+    setIntervalForUpdate() {
       let abc = this;
 
       this.timer = setInterval(() => {
-        //console.log(_this)
-        // console.log(this)
-        //console.log(_this.option.series.);//["data"][0]++;
-        //console.log(JSON.parse(JSON.stringify(_this.option.series)));
-
         // 获取真实数据
         //console.log(JSON.parse(JSON.stringify(_this.option.series[0])));
 
@@ -85,9 +142,18 @@ export default {
         let handler = function(response) {
           let _this = abc;
           //console.log(response)
-          _this.option.series[0].data.shift()
-          _this.option.series[0].data.push(_this.packTimeAndData(response.data["now_bytes"]));
 
+          let second_num = response.data["second_num"];
+          let second_bytes = response.data["second_bytes"];
+
+          _this.option_for_second_num.series[0].data.shift();
+          _this.option_for_second_num.series[0].data.push(_this.packTimeAndData(second_num));
+
+          _this.option_for_second_bytes.series[0].data.shift();
+          _this.option_for_second_bytes.series[0].data.push(_this.packTimeAndData(second_bytes));
+
+          _this.now_num = response.data["now_num"] + " 个";
+          _this.now_bytes = _this.readablizeBytes(response.data["now_bytes"]);
         };
         axios
             .get("/api/counter")
@@ -102,12 +168,13 @@ export default {
       let now = new Date();
       for (let i = -100; i < 0; i++) {
         let time = new Date(now.getTime() + i * 1000);
-        console.log(time)
-        this.option.series[0].data.push([time,0]);
+        this.option_for_second_num.series[0].data.push([time, 0]);
+        this.option_for_second_bytes.series[0].data.push([time, 0]);
       }
     }
     ,
     packTimeAndData(value) {
+      // 把当前时间与数据结合在一起作为一个数据点
       let now = new Date();
       return {
         name: now.toString(),
@@ -116,8 +183,14 @@ export default {
           value
         ]
       };
+    },
+    readablizeBytes(bytes) {
+      var s = ["Bytes", "KB", "MB", "GB", "TB", "PB"];
+      var e = Math.floor(Math.log(bytes) / Math.log(1024));
+      return (bytes / Math.pow(1024, Math.floor(e))).toFixed(2) + " " + s[e];
     }
   }
+
 
 };
 </script>
